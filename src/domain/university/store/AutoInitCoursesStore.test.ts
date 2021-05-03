45c08bd3eed882d6c830a7248993d7e7c23a54da
jonki <andrzej.fricze@gmail.com>
2021-05-03 14:26:42 +0200

    Mock onBecomeObserved for testing purposes

--- src/domain/university/store/AutoInitCoursesStore.test.ts
+++ src/domain/university/store/AutoInitCoursesStore.test.ts
@@ -1,10 +1,33 @@
 import { CoursesStore } from "./AutoInitCoursesStore";
 import { getCoursesEmpty, getCourses } from "./test/mockGetCourses";
 
+jest.mock("mobx", () => ({
+  ...jest.requireActual("mobx"),
+  onBecomeObserved: jest
+    .fn()
+    .mockImplementation((_instance, _property, handler) => {
+      // call onBecomeObserved handler immediately in constructor, not to wait
+      // for a property being observed.
+      // it's a tricky solution, since we're overriding mobx lifecycle mechanism,
+      // but might actually be useful for testing
+      handler();
+    }),
+}));
+
 test("Store has proper initial value", () => {
-  const store = new CoursesStore(getCourses);
+  const store = new CoursesStore(getCourses, false);
 
   expect(store.courses).toEqual([]);
+  expect(store.nextPageAvailable).toEqual(true);
+});
+
+test("Store gets first page properly loaded after onBecomeObserved handler is called", () => {
+  const store = new CoursesStore(getCourses, true);
+
+  setImmediate(() => {
+    expect(store.courses.length).toEqual(3);
+    expect(store.nextPageAvailable).toEqual(true);
+  });
 });
 
 test("fetchMore gets data from getCourses", () => {
